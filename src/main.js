3219fffe8b392c10b1d04514085a1a0a63cd1115
Mr-GJ <997794259@qq.com>
2021-07-19 14:33:32 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css';
+import {AddressEdit ,AddressList ,Icon,SubmitBar,Stepper,Step, Steps,SwipeCell ,Checkbox, CheckboxGroup,Field ,Form,Tag,Button,Image as VanImage,ImagePreview ,Card ,Swipe, SwipeItem, Lazyload, Badge, Sidebar, SidebarItem, Collapse, CollapseItem, Tab, Tabs} from 'vant';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(Swipe).use(SwipeItem).use(Lazyload, {
+    loading: require('./assets/images/default.jpg')
+}).use(Badge).use(Sidebar).use(SidebarItem).use(Collapse).use(CollapseItem)
+    .use(Tab).use(Tabs).use(Card).use(ImagePreview).use(VanImage).use(Tag)
+    .use(Button).use(Form).use(Field).use(Checkbox).use(CheckboxGroup)
+    .use(SwipeCell).use(Step).use(Steps).use(Stepper).use(SubmitBar).use(Icon)
+    .use(AddressList).use(AddressEdit)
+    .use(store).use(router).mount('#app')
